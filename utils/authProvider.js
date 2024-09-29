ff942b072cfadbc65b6cad5e92481f143484bb37
Md Arshad <mdarshd246@gmail.com>
2024-09-30 00:27:04 +0530

    added callback in authProvider.js

--- utils/authProvider.js
+++ utils/authProvider.js
@@ -1,3 +1,5 @@
+import dbConnected from "@/config/database";
+import User from "@/models/User";
 import GoogleProvider from "next-auth/providers/google";
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET_KEY } from "./constant";
 
@@ -18,20 +20,41 @@ export const authOptions = {
     // ...add more providers here
   ],
   callbacks: {
-    // invoke on successful sign in 
+    // invoke on successful sign in
     async signIn({ profile }) {
-        // 1. connect to the database
-        // 2. check user is already or not
-        // 3. if not, create new user
-        // 4. return true to allow sign up 
+      console.log(profile);
+
+      // 1. connect to the database
+      await dbConnected();
+
+      // 2. check user is already or not
+      const user = await User.findOne({ email: profile.email });
+
+      // 3. if not, create new user
+      if (!user) {
+        // Truncate user name if too long
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
+      // 4. return true to allow sign up
+      return true;
     },
 
-    // modify the session 
-    async session({session}){
-        // 1. get user id from the database
-        // 2. assign the user id to the session
-        // 3. return session
-    }
+    // modify the session
+    async session({ session }) {
+      console.log(session);
+
+      // 1. get user id from the database
+      const user = await User.findOne({ email: session.user.email });
+      // 2. assign the user id to the session
+      session.user.id = user._id.toString();
+      // 3. return session
+      return session;
+    },
   },
 };
-
